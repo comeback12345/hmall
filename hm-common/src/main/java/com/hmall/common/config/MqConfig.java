07b33d0a08226237e92dd9f663f8f2b3f6ee82b1
comeback12345 <1827896640@qq.com>
2024-11-04 15:30:05 +0800

    更新

--- hm-common/src/main/java/com/hmall/common/config/MqConfig.java
+++ hm-common/src/main/java/com/hmall/common/config/MqConfig.java
@@ -2,9 +2,12 @@ package com.hmall.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmall.common.utils.RabbitMqHelper;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,8 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(value = {MessageConverter.class, RabbitTemplate.class})
 public class MqConfig {
 
+    private static final int INITIAL_CONCURRENT_CONSUMERS = 10;
+    private static final int MAX_CONCURRENT_CONSUMERS = 10;
     @Bean
     @ConditionalOnBean(ObjectMapper.class)
     public MessageConverter messageConverter(ObjectMapper mapper){
@@ -28,4 +33,16 @@ public class MqConfig {
     public RabbitMqHelper rabbitMqHelper(RabbitTemplate rabbitTemplate){
         return new RabbitMqHelper(rabbitTemplate);
     }
+
+    @Bean("customContainerFactory")
+    public SimpleRabbitListenerContainerFactory containerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConcurrentConsumers(INITIAL_CONCURRENT_CONSUMERS); //设置线程数
+        factory.setMaxConcurrentConsumers(MAX_CONCURRENT_CONSUMERS); //最大线程数
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
 }
