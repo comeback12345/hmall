535595efa2d857feee4acae7c0da20c9c6dff197
comeback12345 <1827896640@qq.com>
2024-11-13 12:04:55 +0800

    完善功能

--- user-service/src/main/java/com/hmall/user/domain/query/UserPageQuery.java
+++ user-service/src/main/java/com/hmall/user/domain/query/UserPageQuery.java
@@ -1,23 +1,31 @@
 package com.hmall.user.domain.query;
 
 import com.hmall.common.domain.PageQuery;
+import com.hmall.user.enums.UserStatus;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDateTime;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel(description = "商品分页查询条件")
 public class UserPageQuery extends PageQuery {
-    @ApiModelProperty("搜索关键字")
-    private String key;
-    @ApiModelProperty("商品分类")
-    private String category;
-    @ApiModelProperty("商品品牌")
-    private String brand;
-    @ApiModelProperty("价格最小值")
-    private Integer minPrice;
-    @ApiModelProperty("价格最大值")
-    private Integer maxPrice;
+
+    @ApiModelProperty("用户id")
+    private Long id;
+    @ApiModelProperty("用户名")
+    private String name;
+    @ApiModelProperty("用户手机号")
+    private String phone;
+    @ApiModelProperty("用户状态")
+    private UserStatus status;
+    @ApiModelProperty("连续失败次数")
+    private int numberofConsecutiveFailures;
+    @ApiModelProperty("最近登录失败时间")
+    private LocalDateTime LoginFailedfulTime;
+    @ApiModelProperty("最近登录成功时间")
+    private LocalDateTime LoginSuccessfulTime;
 }
